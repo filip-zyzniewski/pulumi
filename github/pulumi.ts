41a71921affab403bc941fe026f52abb02493a8f
Filip Zyzniewski <filip@taxamo.com>
2021-05-02 13:57:41 +0200

    github: first resource - the pulumi repository

---
+++ github/pulumi.ts
@@ -0,0 +1,14 @@
+import * as github from "@pulumi/github";
+
+const repo = new github.Repository(
+    "pulumi",
+    {
+        description: "Pulumi code that I use to manage my infrastructure",
+        allowSquashMerge: false,
+        deleteBranchOnMerge: true,
+        hasIssues: true,
+        hasProjects: true,
+        hasWiki: false,
+        visibility: "public",
+    },
+);
