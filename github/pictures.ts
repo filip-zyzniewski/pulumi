738ec45d82917303ef552ae9d8b0f7667423e38a
Filip Zyzniewski <filip.zyzniewski@gmail.com>
2021-05-23 15:22:29 +0200

    github: do not uniquize the repo names
    It doesn't look good enough.

--- github/pictures.ts
+++ github/pictures.ts
@@ -3,6 +3,7 @@ import * as github from "@pulumi/github";
 const repo = new github.Repository(
     "pictures",
     {
+        name: "pictures",
         description: "My attempts at photography with code",
         allowSquashMerge: false,
         deleteBranchOnMerge: true,
--- github/pulumi.ts
+++ github/pulumi.ts
@@ -3,6 +3,7 @@ import * as github from "@pulumi/github";
 const repo = new github.Repository(
     "pulumi",
     {
+        name: "pulumi",
         description: "Pulumi code that I use to manage my infrastructure",
         allowSquashMerge: false,
         deleteBranchOnMerge: true,
